b04227c32fb145462bb3185ba2b6baa6a134c596
Emnardifi <amna.rdifi@gmail.com>
2025-01-31 14:57:16 +0100

    produit

---
+++ produit.py
@@ -0,0 +1,3 @@
+#fonction produit qui retourne le produit de deux parametres
+def prod(a,b):
+    return a*b
---
+++ test_prod.py
@@ -0,0 +1,4 @@
+#this is a test for the function produit
+from produit import prod 
+def test_Prod():
+    assert prod(5,9)==25
