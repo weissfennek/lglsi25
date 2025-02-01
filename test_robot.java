c47a448d515328e201f98885cc40ce8a5c786a95
teyssir brddi <teyssirbrddi8@gmail.com>
2025-02-01 20:47:13 +0100

    robot

---
+++ test_robot.java
@@ -0,0 +1,25 @@
+public class test_robot {
+  
+
+
+	
+		public static void main(String[] args) {
+			Robot robot =new Robot(10,150,3);
+			robot.afficher();
+			
+			robot.tourner();
+			robot.afficher();
+			robot.tourner();
+			robot.afficher();
+			
+			robot.deplacer();
+			robot.deplacer();
+			robot.afficher();
+			
+			
+		}
+
+	}
+
+    
+
