f79e20deed2e105843f8279bd57e5f082ffa994a
Taycir37 <taycir.benrhouma24@gmail.com>
2025-02-01 12:33:42 +0100

    testinverse.py

---
+++ testinverse.py
@@ -0,0 +1,5 @@
+def inverser_chaine(chaine):
+    return chaine[::-1]
+chaine = input("Entrez une chaîne : ")
+chaine_inversee = inverser_chaine(chaine)
+print("La chaîne inversée est :", chaine_inversee)
